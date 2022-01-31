25ea1d42b3d22ac7f95afb0eba3151249fc1b2ed
maddiesmac <madeline.sem@bison.howard.edu>
2022-01-31 17:05:18 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    System.out.println("What is your name?");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+    System.out.println("Hello, " + name);
+    }
+}
